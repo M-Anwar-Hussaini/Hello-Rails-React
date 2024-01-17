3f3f7cd0e842ddbb8becdb49432c8cab6bd54ca0
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-01-17 21:29:36 +0430

    Add application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import store from "./redux/store";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Greeting from "./components/Greeting";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Greeting/>}/>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+)
